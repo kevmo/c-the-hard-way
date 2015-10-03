488b8c58dabb672eecc5498f876ee399ecbaf75a
kevmo <k.james.moore@gmail.com>
2015-10-03 11:32:42 -0400

    Intro to pointers.

---
+++ intro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	// int a;
+	// int *p = &a;
+
+	// print("When you just print `a`: %i\n", a);
+	// printf("The location of a using `&a`: %i\n", &a);
+
+	return 1;
+}
